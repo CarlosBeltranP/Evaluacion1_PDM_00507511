f1b4653b535a043d6092ab77ffcd49588ae66a12
Dona <00084417@uca.edu.sv>
2019-03-29 15:51:38 -0600

    casi terminado3

--- app/src/main/java/com/example/evaluacion1/MainActivity.java
+++ app/src/main/java/com/example/evaluacion1/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.evaluacion1.utils.AppConstants;
@@ -15,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText correoET;
     private Button enviarBT;
 
-    private TextView bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9;
+    private LinearLayout bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9;
+    private TextView tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9;
     private int cont1,cont2, cont3, cont4, cont5, cont6, cont7, cont8, cont9;
     private int total;
 
@@ -25,71 +27,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt1 = findViewById(R.id.et_1);
-        bt2 = findViewById(R.id.et_2);
-        bt3 = findViewById(R.id.et_3);
-        bt4 = findViewById(R.id.et_4);
-        bt5 = findViewById(R.id.et_5);
-        bt6 = findViewById(R.id.et_6);
-        bt7 = findViewById(R.id.et_7);
-        bt8 = findViewById(R.id.et_8);
-        bt9 = findViewById(R.id.et_9);
+        bt1 = findViewById(R.id.bt_1);
+        bt2 = findViewById(R.id.bt_2);
+        bt3 = findViewById(R.id.bt_3);
+        bt4 = findViewById(R.id.bt_4);
+        bt5 = findViewById(R.id.bt_5);
+        bt6 = findViewById(R.id.bt_6);
+        bt7 = findViewById(R.id.bt_7);
+        bt8 = findViewById(R.id.bt_8);
+        bt9 = findViewById(R.id.bt_9);
+
+        tv1 = findViewById(R.id.et_1);
+        tv2 = findViewById(R.id.et_2);
+        tv3 = findViewById(R.id.et_3);
+        tv4 = findViewById(R.id.et_4);
+        tv5 = findViewById(R.id.et_5);
+        tv6 = findViewById(R.id.et_6);
+        tv7 = findViewById(R.id.et_7);
+        tv8 = findViewById(R.id.et_8);
+        tv9 = findViewById(R.id.et_9);
+
 
 
 
         bt1.setOnClickListener(v->{
             cont1++;
             total++;
-            bt1.setText(cont1 + "");
+            tv1.setText(cont1 + "");
 
                 });
 
         bt2.setOnClickListener(v->{
             cont2++;
 
-            bt2.setText(cont2 + "");
+            tv2.setText(cont2 + "");
 
         });
 
         bt3.setOnClickListener(v->{
             cont3++;
 
-            bt3.setText(cont3 + "");
+            tv3.setText(cont3 + "");
 
         });
 
         bt4.setOnClickListener(v->{
             cont4++;
-            bt4.setText(cont4 + "");
+            tv4.setText(cont4 + "");
 
         });
 
         bt5.setOnClickListener(v->{
             cont5++;
-            bt5.setText(cont5 + "");
+            tv5.setText(cont5 + "");
 
         });
         bt6.setOnClickListener(v->{
             cont6++;
-            bt6.setText(cont6 + "");
+            tv6.setText(cont6 + "");
 
         });
 
         bt7.setOnClickListener(v->{
             cont7++;
-            bt7.setText(cont7 + "");
+            tv7.setText(cont7 + "");
 
         });
 
         bt8.setOnClickListener(v->{
             cont8++;
-            bt8.setText(cont8 + "");
+            tv8.setText(cont8 + "");
 
         });
 
         bt9.setOnClickListener(v->{
             cont9++;
-            bt9.setText(cont9 + "");
+            tv9.setText(cont9 + "");
 
         });
 
